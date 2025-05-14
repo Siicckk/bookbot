040d923ff34ee36ef83b850464759ef7debef968
siicckk <shredgodnoah@gmail.com>
2025-05-14 05:42:06 +0000

    A: word count stats.py created

---
+++ main.py
@@ -0,0 +1,10 @@
+from stats import num_words 
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    count = num_words(book_text)
+    print(f"{count} words found in the document")
+main()    
---
+++ stats.py
@@ -0,0 +1,4 @@
+def num_words(book_text):
+    words = book_text.split()
+    return len(words)
+
